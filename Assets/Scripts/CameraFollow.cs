909e58f07201ebbe3a02a8ea503bc5dfbdfdf458
cimbyote <cimbyote@users.noreply.github.com>
2019-05-24 11:01:31 -0500

    Added Projectile to my game for demo

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,29 +8,17 @@ public class CameraFollow : MonoBehaviour
     public GameObject LevelStart;
     public GameObject LevelEnd;
 
-    public GameObject LevelTop;
-    public GameObject LevelBottom;
-
     float xMin;
     float xMax;
 
-    float yMin;
-    float yMax;
-
     float CamPos;
-    float CamPosY;
 
     [Range(-20, 20)]public float xMinThreshold;
     [Range(-20, 20)]public float xMaxThreshold;
-    [Range(-20, 20)]public float yMinThreshold;
-    [Range(-20, 20)]public float yMaxThreshold;
-
     private void Start() {
         xMin = LevelStart.transform.position.x + xMinThreshold;
         xMax = LevelEnd.transform.position.x - xMaxThreshold;
 
-        yMax = LevelTop.transform.position.y + yMaxThreshold;
-        yMin = LevelBottom.transform.position.y - yMinThreshold;
     }
 
     // Update is called once per frame
@@ -40,10 +28,7 @@ public class CameraFollow : MonoBehaviour
             CamPos = player.transform.position.x;
             CamPos = Mathf.Clamp(CamPos, xMin, xMax);
 
-            CamPosY = player.transform.position.y;
-            CamPosY = Mathf.Clamp(CamPosY, yMin, yMax);
-
-            this.transform.position = new Vector3(CamPos, CamPosY, -10);
+            this.transform.position = new Vector3(CamPos, this.transform.position.y, -10);
         }
         else
         {
