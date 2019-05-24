be367615014ad723d5c783aa68b4ec20064186b2
cimbyote <cimbyote@users.noreply.github.com>
2019-05-24 10:48:02 -0500

    Double Mushroom Bug Fix

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,14 +8,29 @@ public class CameraFollow : MonoBehaviour
     public GameObject LevelStart;
     public GameObject LevelEnd;
 
+    public GameObject LevelTop;
+    public GameObject LevelBottom;
+
     float xMin;
     float xMax;
 
+    float yMin;
+    float yMax;
+
     float CamPos;
+    float CamPosY;
+
+    [Range(-20, 20)]public float xMinThreshold;
+    [Range(-20, 20)]public float xMaxThreshold;
+    [Range(-20, 20)]public float yMinThreshold;
+    [Range(-20, 20)]public float yMaxThreshold;
 
     private void Start() {
-        xMin = LevelStart.transform.position.x + 7f;
-        xMax = LevelEnd.transform.position.x - 7f;
+        xMin = LevelStart.transform.position.x + xMinThreshold;
+        xMax = LevelEnd.transform.position.x - xMaxThreshold;
+
+        yMax = LevelTop.transform.position.y + yMaxThreshold;
+        yMin = LevelBottom.transform.position.y - yMinThreshold;
     }
 
     // Update is called once per frame
@@ -24,7 +39,11 @@ public class CameraFollow : MonoBehaviour
         if(player){
             CamPos = player.transform.position.x;
             CamPos = Mathf.Clamp(CamPos, xMin, xMax);
-            this.transform.position = new Vector3(CamPos, this.transform.position.y, -10);
+
+            CamPosY = player.transform.position.y;
+            CamPosY = Mathf.Clamp(CamPosY, yMin, yMax);
+
+            this.transform.position = new Vector3(CamPos, CamPosY, -10);
         }
         else
         {
--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -55,7 +55,11 @@ public class PlayerHealthManager : MonoBehaviour
     public void GiveHealth()
     {
         playerHealth = 2;
-        TransitionBig();
+        if(state == PlayerState.little)
+        {
+            TransitionBig();
+        }
+        
     }
 
     private void TransitionBig()
